cc3e735c72a0dc4617abc3fcd19419b0fbd1e111
vongkhmer <vutheavong.kh@gmail.com>
2020-11-04 23:45:38 +0900

    Add solution to the problem F in CF667 Div3

--- CodeForce/CF667Div3/F.cpp
+++ CodeForce/CF667Div3/F.cpp
@@ -3,81 +3,95 @@
 #include <array>
 using namespace std;
 
-constexpr int mxN = 200;
+constexpr int mxN = 201;
+//dp[i][j][k] : maximum appearance of t in s[0..i] using no more than j moves
+//while having k appearance of t[0]
+// dp[i][j][k] = -1 if such case is impossible 
 int dp[mxN][mxN][mxN];
+string s, t;
 
-void update_max(int& a, int b){
+void umax(int& a, int b){
+    //update value of a if b > a
     a = max(a, b);
+}
+
+int cal(int i, int j, int k){
+    //i , j , k cannot be negative
+    if( i < 0 || j < 0 || k < 0) return -1;
+    // it is impossible to appearance more than the total character
+    if( k > i + 1) return -1;
+    //if calculated, just return the result
+    if( dp[i][j][k] > -1e9) return dp[i][j][k];
+
+    int ans = -1; 
+
+    if(s[i] == t[1]){
+        int e = (t[1] == t[0]) ? 1 : 0;
+
+        int no_update_prev_state = cal(i-1, j , k - e) ; 
+        if( no_update_prev_state > - 1){
+            //no update, use it now
+            umax(ans, no_update_prev_state  + k - e);        
+        }
+        
+        int update_t0_prev_state = cal(i-1, j - 1 + e , k - 1);
+        if( update_t0_prev_state > -1){
+            //update s[i] to t[0], for future use
+            umax(ans, update_t0_prev_state + e*(k-1));
+        }
+    }else if( s[i] == t[0]){
+        // no update = keep it for future use
+        int no_update_prev_state = cal(i - 1, j, k - 1);
+        if(no_update_prev_state > -1){
+            umax(ans, no_update_prev_state);
+        }
+        //update to t[1], use it now
+        int update_t1_prev_state = cal(i - 1, j - 1, k);
+        if( update_t1_prev_state > -1){
+            umax(ans, update_t1_prev_state + k);
+        }
+    }else{
+        int e = (t[0] == t[1]) ? 1 : 0;
+        //no update
+        int no_update_prev_state = cal(i - 1, j, k);
+        umax(ans, no_update_prev_state); 
+
+        //update to t[0]
+        int update_t0_prev_state = cal(i - 1, j - 1, k - 1);
+        if( update_t0_prev_state > -1){
+            umax(ans, update_t0_prev_state + e *( k - 1));
+        }
+
+        //update to t[1]
+        int update_t1_prev_state = cal(i - 1, j - 1, k -e);
+        if(update_t1_prev_state  > -1){
+            umax(ans, update_t1_prev_state + k - e);
+        }
+    }
+
+    dp[i][j][k] = ans;
+    return ans;
 }
 
 void solve(){
     int n, mv;
     cin >> n >> mv;
-    string s, t;
     cin >> s >> t;
-    const int INF = 1e9;
-
-    for(int i = 0 ; i < mxN; i++){
-        for(int j = 0; j < mxN; j++){
-            for(int k = 0 ; k < mxN; k++){
-                dp[i][j][k] = -INF;
-            }
-        }
-    }
-    if(s[0] == t[0]){
-        dp[0][0][1] = 0;
-        dp[0][1][0] = 0;
-        dp[0][1][1] = 0;
-    }else{
-        dp[0][0][0] = 0;
-        dp[0][1][0] = 0;
-        dp[0][0][1] = 0;
-    }
-    for(int i = 1; i < n ; i++){
-        for(int k = 0 ; k <= i + 1; k++){
-            int j = 0;
-            dp[i][j][k] = dp[i][j][k-1] + (s[i] == t[0]);
-        }
-    }
-
-    for(int i = 1 ; i < n; i++){
-        for(int j = 1 ; j <= mv; j++){
+    for(int i = 1 ; i < n ; i++){
+        for(int j = 0 ; j <= mv; j++){
             for(int k = 0 ; k <= i + 1 ; k++){
-                int e0 = (s[i] == t[0]) ? 1 : 0;
-                int e1 = (s[i] == t[1]) ? 1 : 0;
-                int e2 = (e0 && e1);
-
-                if(e2){
-                    update_max(dp[i][j][k], dp[i][j][k-1] + k - 1);
-                }else{
-                    if(e0){
-                        // not changing s[i]
-                        update_max(dp[i][j][k], dp[i-1][j][k-1] * 2);
-                        //change s[i] to t1 
-                        update_max(dp[i][j][k], dp[i - 1][j-1][k] + k - 1);
-                    }else if(e1){
-                        //not changing s[i]
-                        update_max(dp[i][j][k], dp[i - 1][j][k] + k);
-                        //change s[i] to t0
-                        update_max(dp[i][j][k], dp[i - 1][j-1][k-1]);
-                    }else{
-                        //no change
-                        update_max(dp[i][j][k], dp[i-1][j][k]);
-                        //change s[i] to t0
-                        update_max(dp[i][j][k], dp[i-1][j-1][k-1]);
-                        //change s[i] to t1
-                        update_max(dp[i][j][k], dp[i-1][j-1][k] + k);
-                    }
-                }
-                printf("i %d j %d k %d dp %d\n", i , j , k , dp[i][j][k]);
+                //-1e9 means not yet calculated
+                dp[i][j][k] = -1e9;
             }
         }
     }
+    int e = (s[0] != t[0]) ? 1 : 0;
+    // -1 means impossible
+    dp[0][0][e] = -1;
+
     int ans = 0;
-    for(int j = 0 ; j <= mv ; j++){
-        for(int k = 0 ; k <= n ; k++){
-            update_max(ans, dp[n-1][j][k]);
-        }
+    for(int k = 0 ; k <= n ; k++){
+        umax(ans, cal(n-1, mv, k));
     }
     cout << ans << "\n";
 }
